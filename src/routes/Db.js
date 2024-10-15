528799f72696fe2dd15846e32fcdd699b671bb98
Kumar-vishal00021 <vishalkumar@Vishals-MacBook-Air.local>
2024-10-15 18:18:34 +0530

    using Atlas cloud

--- src/routes/Db.js
+++ src/routes/Db.js
@@ -5,13 +5,7 @@ const url = 'mongodb+srv://Kumar-vishal00021:Vishal@1234@e-commerce.h5vsy.mongod
 let client;
 export default async function connectionToDb(){
     try {
-        client = await MongoClient.connect(url, {
-            tls: true, // Use TLS for connection
-            tlsInsecure: true, // Allows connection even with self-signed certificates
-            tlsAllowInvalidCertificates: true, // Allows invalid certificates (only for development)
-            useUnifiedTopology: true, // Ensures MongoDB driver manages connections efficiently
-            connectTimeoutMS: 10000 // Time out after 10 seconds
-        });
+        client = await MongoClient.connect(url);
         console.log("Db connected successfully");
     } catch (err) {
         console.error("Connection error: ", err);
