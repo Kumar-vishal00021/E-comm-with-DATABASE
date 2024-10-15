2a2050daa7fc0e3814e58afc3ac46e7cc1786820
Kumar-vishal00021 <vishalkumar@Vishals-MacBook-Air.local>
2024-10-15 18:01:57 +0530

    using Atlas cloud

--- src/routes/Db.js
+++ src/routes/Db.js
@@ -1,21 +1,22 @@
 import { MongoClient } from "mongodb";
 const dbName = 'e-comm-using-rest-api';
-const url = 'mongodb+srv://Kumar-vishal00021:Vishal%401234@e-commerce.h5vsy.mongodb.net/';
+const url = 'mongodb+srv://Kumar-vishal00021:Vishal%401234@e-commerce.h5vsy.mongodb.net/?ssl=true&tls=true';
 
 let client;
-export default function connectionToDb(){
-    MongoClient.connect(url, {
-        ssl: true,  
-        tlsAllowInvalidCertificates: true 
-    })
-    .then((currentClient)=>{
-        client=currentClient;
+export default async function connectionToDb(){
+    try {
+        client = await MongoClient.connect(url, {
+            tls: true, // Use TLS for connection
+            tlsInsecure: true, // Allows connection even with self-signed certificates
+            tlsAllowInvalidCertificates: true, // Allows invalid certificates (only for development)
+            useUnifiedTopology: true, // Ensures MongoDB driver manages connections efficiently
+            connectTimeoutMS: 10000 // Time out after 10 seconds
+        });
         console.log("Db connected successfully");
-    })
-    .catch((err)=>{
-        console.log(err);
-        process.exit (1);
-    })
+    } catch (err) {
+        console.error("Connection error: ", err);
+        process.exit(1);
+    }
 }
 export  function Db(){
     return client.db(dbName);
